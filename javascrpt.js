b55024903fbe5ae1ba92f66500f90bbd50c4946f
nser26n <nser26n@gmail.com>
2024-04-26 00:26:36 +0300

    this push for test the more bransh

---
+++ javascrpt.js
@@ -0,0 +1,8 @@
+
+// var button = document.querySelector(".chick");
+// function change() {
+//     document.body.style.backgroundColor = "#2196f3";
+//     document.getElementById("test").innerText = " nserallh almoflhi";
+    
+// }
+// button.addEventListener("click", change);
